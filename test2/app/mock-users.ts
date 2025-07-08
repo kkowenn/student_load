f7df7de7f6fb6d3e4082618354148e4e3fd51163
kritsada kruapat <89009250+kkowenn@users.noreply.github.com>
2025-07-08 16:13:54 +0700

    Push test2/app/mock-users.ts via Student Loan App

---
+++ test2/app/mock-users.ts
@@ -0,0 +1,7 @@
+import { User, UserRole } from "./types";
+
+// Mock users
+export const mockUsers: User[] = [
+  { id: "1", name: "สมชาย นักศึกษา", role: "student" },
+  { id: "2", name: "สมหญิง แอดมิน", role: "admin" },
+];
